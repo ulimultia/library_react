24a1841426c2f28a8fa58c71064c2a91ac20bc03
ulimultia <43207525+ulimultia@users.noreply.github.com>
2021-03-24 15:40:51 +0700

    layout dashboard

---
+++ src/components/WidgetDashboard/index.js
@@ -0,0 +1,38 @@
+import React, { useState } from 'react';
+import { Col, Row, Button, Badge, Card, CardHeader, CardTitle, CardBody, CardFooter } from 'reactstrap';
+
+const WidgetDashboard = (props) => {
+  const { icon, kategori, info, footer} = props;
+
+  return (
+    <div>
+        <Card className="card-stats">
+            <CardBody>
+                <Row>
+                <Col md="4" xs="5">
+                    <div className="icon-big text-center icon-warning">
+                    {/* <i className="nc-icon nc-money-coins"  style={{color:"#825e40"}}/> */}
+                    <i className={icon}  style={{color:"#825e40"}}/>
+                    </div>
+                </Col>
+                <Col md="8" xs="7">
+                    <div className="numbers">
+                    <p className="card-category">{kategori}</p>
+                    <CardTitle tag="p">{info}</CardTitle>
+                    <p />
+                    </div>
+                </Col>
+                </Row>
+            </CardBody>
+            <CardFooter>
+                <hr />
+                <div className="stats">{footer}</div>
+            </CardFooter>
+        </Card>
+    </div>
+  );
+}
+
+export default WidgetDashboard;
+
+
--- src/views/Peminjam/Dashboard.js
+++ src/views/Peminjam/Dashboard.js
@@ -1,33 +1,10 @@
-/*!
-
-=========================================================
-* Paper Dashboard React - v1.2.0
-=========================================================
-
-* Product Page: https://www.creative-tim.com/product/paper-dashboard-react
-* Copyright 2020 Creative Tim (https://www.creative-tim.com)
-
-* Licensed under MIT (https://github.com/creativetimofficial/paper-dashboard-react/blob/master/LICENSE.md)
-
-* Coded by Creative Tim
-
-=========================================================
-
-* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
-
-*/
 import React from "react";
 // react plugin used to create charts
 import { Line, Pie } from "react-chartjs-2";
 // reactstrap components
 import {
-  Card,
-  CardHeader,
-  CardBody,
-  CardFooter,
-  CardTitle,
-  Row,
-  Col,
+  Card,CardHeader,CardBody,CardFooter,CardTitle, CardText, CardImgOverlay, CardImg,
+  Row,Col,
 } from "reactstrap";
 // core components
 import {
@@ -35,6 +12,8 @@ import {
   dashboardEmailStatisticsChart,
   dashboardNASDAQChart,
 } from "variables/charts.js";
+import WidgetDashboard from "components/WidgetDashboard/"
+import BukuRekomendasi from "components/BukuRekomendasiDashboard/index"
 
 class Dashboard extends React.Component {
   render() {
@@ -43,186 +22,142 @@ class Dashboard extends React.Component {
         <div className="content">
           <Row>
             <Col lg="3" md="6" sm="6">
-              <Card className="card-stats">
-                <CardBody>
-                  <Row>
-                    <Col md="4" xs="5">
-                      <div className="icon-big text-center icon-warning">
-                        <i className="nc-icon nc-globe text-warning" />
-                      </div>
-                    </Col>
-                    <Col md="8" xs="7">
-                      <div className="numbers">
-                        <p className="card-category">Capacity</p>
-                        <CardTitle tag="p">150GB</CardTitle>
-                        <p />
-                      </div>
-                    </Col>
-                  </Row>
-                </CardBody>
-                <CardFooter>
-                  <hr />
-                  <div className="stats">
-                    <i className="fas fa-sync-alt" /> Update Now
-                  </div>
-                </CardFooter>
-              </Card>
+              <WidgetDashboard
+                icon = "fas fa-money-bill"
+                kategori = "Rp"
+                info = "56700,-"
+                footer ="SALDO"
+              />
             </Col>
             <Col lg="3" md="6" sm="6">
-              <Card className="card-stats">
-                <CardBody>
-                  <Row>
-                    <Col md="4" xs="5">
-                      <div className="icon-big text-center icon-warning">
-                        <i className="nc-icon nc-money-coins text-success" />
-                      </div>
-                    </Col>
-                    <Col md="8" xs="7">
-                      <div className="numbers">
-                        <p className="card-category">Revenue</p>
-                        <CardTitle tag="p">$ 1,345</CardTitle>
-                        <p />
-                      </div>
-                    </Col>
-                  </Row>
-                </CardBody>
-                <CardFooter>
-                  <hr />
-                  <div className="stats">
-                    <i className="far fa-calendar" /> Last day
-                  </div>
-                </CardFooter>
-              </Card>
+              <WidgetDashboard
+                icon = "fas fa-coins"
+                kategori = "Rp"
+                info = "3300,-"
+                footer ="DENDA"
+              />
             </Col>
             <Col lg="3" md="6" sm="6">
-              <Card className="card-stats">
-                <CardBody>
-                  <Row>
-                    <Col md="4" xs="5">
-                      <div className="icon-big text-center icon-warning">
-                        <i className="nc-icon nc-vector text-danger" />
-                      </div>
-                    </Col>
-                    <Col md="8" xs="7">
-                      <div className="numbers">
-                        <p className="card-category">Errors</p>
-                        <CardTitle tag="p">23</CardTitle>
-                        <p />
-                      </div>
-                    </Col>
-                  </Row>
-                </CardBody>
-                <CardFooter>
-                  <hr />
-                  <div className="stats">
-                    <i className="far fa-clock" /> In the last hour
-                  </div>
-                </CardFooter>
-              </Card>
+              <WidgetDashboard
+                icon = "fas fa-hourglass-half"
+                kategori = "Buku"
+                info = "4"
+                footer ="SEDANG DISEWA"
+              />
             </Col>
             <Col lg="3" md="6" sm="6">
-              <Card className="card-stats">
-                <CardBody>
-                  <Row>
-                    <Col md="4" xs="5">
-                      <div className="icon-big text-center icon-warning">
-                        <i className="nc-icon nc-favourite-28 text-primary" />
-                      </div>
-                    </Col>
-                    <Col md="8" xs="7">
-                      <div className="numbers">
-                        <p className="card-category">Followers</p>
-                        <CardTitle tag="p">+45K</CardTitle>
-                        <p />
-                      </div>
-                    </Col>
-                  </Row>
-                </CardBody>
-                <CardFooter>
-                  <hr />
-                  <div className="stats">
-                    <i className="fas fa-sync-alt" /> Update now
-                  </div>
-                </CardFooter>
-              </Card>
+              <WidgetDashboard
+                icon = "fas fa-book"
+                kategori = "Buku"
+                info = "123"
+                footer ="TOTAL BUKU DISEWA"
+              />
             </Col>
           </Row>
-          <Row>
-            <Col md="12">
-              <Card>
-                <CardHeader>
-                  <CardTitle tag="h5">Users Behavior</CardTitle>
-                  <p className="card-category">24 Hours performance</p>
-                </CardHeader>
-                <CardBody>
-                  <Line
-                    data={dashboard24HoursPerformanceChart.data}
-                    options={dashboard24HoursPerformanceChart.options}
-                    width={400}
-                    height={100}
-                  />
-                </CardBody>
-                <CardFooter>
-                  <hr />
-                  <div className="stats">
-                    <i className="fa fa-history" /> Updated 3 minutes ago
-                  </div>
-                </CardFooter>
-              </Card>
+          <Row className="mt-4">
+            <Col xs="2" sm="4"><hr/></Col>
+            <Col xs="8" sm="4"><h5 className="text-center"><b>Buku Terbaru</b></h5></Col>
+            <Col xs="2" sm="4"><hr/></Col>
+            <Col xs="6" sm="2">
+              <BukuRekomendasi 
+                image = "https://images.wallpaperscraft.com/image/book_bouquet_cup_147482_3840x2160.jpg"
+                kategori = "Novel"
+                genre = "Remaja"
+                judul = "Cinta Pertamaku"
+              />
+            </Col>
+            <Col xs="6" sm="2">
+              <BukuRekomendasi 
+                image = "https://upload.wikimedia.org/wikipedia/id/c/c8/Doraemon_volume_1_cover.jpg"
+                kategori = "Komik"
+                genre = "Series"
+                judul = "Doraemon Volume 1"
+              />
+            </Col>
+            <Col xs="6" sm="2">
+              <BukuRekomendasi 
+                image = "https://upload.wikimedia.org/wikipedia/id/thumb/8/8e/Laskar_pelangi_sampul.jpg/220px-Laskar_pelangi_sampul.jpg"
+                kategori = "Novel"
+                genre = "Sosial Pendidikan"
+                judul = "Laskar Pelangi"
+              />
+            </Col>
+            <Col xs="6" sm="2">
+              <BukuRekomendasi 
+                image = "https://upload.wikimedia.org/wikipedia/id/c/c8/Ayat-Ayat_Cinta.jpg"
+                kategori = "Novel"
+                genre = "Romantis"
+                judul = "Ayat-ayat Cinta"
+              />
+            </Col>
+            <Col xs="6" sm="2">
+              <BukuRekomendasi 
+                image = "https://upload.wikimedia.org/wikipedia/id/thumb/8/8e/Laskar_pelangi_sampul.jpg/220px-Laskar_pelangi_sampul.jpg"
+                kategori = "Novel"
+                genre = "Sosial Pendidikan"
+                judul = "Laskar Pelangi"
+              />
+            </Col>
+            <Col xs="6" sm="2">
+              <BukuRekomendasi 
+                image = "https://upload.wikimedia.org/wikipedia/id/c/c8/Ayat-Ayat_Cinta.jpg"
+                kategori = "Novel"
+                genre = "Romantis"
+                judul = "Ayat-ayat Cinta"
+              />
             </Col>
           </Row>
-          <Row>
-            <Col md="4">
-              <Card>
-                <CardHeader>
-                  <CardTitle tag="h5">Email Statistics</CardTitle>
-                  <p className="card-category">Last Campaign Performance</p>
-                </CardHeader>
-                <CardBody>
-                  <Pie
-                    data={dashboardEmailStatisticsChart.data}
-                    options={dashboardEmailStatisticsChart.options}
-                  />
-                </CardBody>
-                <CardFooter>
-                  <div className="legend">
-                    <i className="fa fa-circle text-primary" /> Opened{" "}
-                    <i className="fa fa-circle text-warning" /> Read{" "}
-                    <i className="fa fa-circle text-danger" /> Deleted{" "}
-                    <i className="fa fa-circle text-gray" /> Unopened
-                  </div>
-                  <hr />
-                  <div className="stats">
-                    <i className="fa fa-calendar" /> Number of emails sent
-                  </div>
-                </CardFooter>
-              </Card>
+          <Row className="mt-4">
+            <Col xs="2" sm="4"><hr/></Col>
+            <Col xs="8" sm="4"><h5 className="text-center"><b>Buku Terpopuler</b></h5></Col>
+            <Col xs="2" sm="4"><hr/></Col>
+            <Col xs="6" sm="2">
+              <BukuRekomendasi 
+                image = "https://images.wallpaperscraft.com/image/book_bouquet_cup_147482_3840x2160.jpg"
+                kategori = "Novel"
+                genre = "Remaja"
+                judul = "Cinta Pertamaku"
+              />
             </Col>
-            <Col md="8">
-              <Card className="card-chart">
-                <CardHeader>
-                  <CardTitle tag="h5">NASDAQ: AAPL</CardTitle>
-                  <p className="card-category">Line Chart with Points</p>
-                </CardHeader>
-                <CardBody>
-                  <Line
-                    data={dashboardNASDAQChart.data}
-                    options={dashboardNASDAQChart.options}
-                    width={400}
-                    height={100}
-                  />
-                </CardBody>
-                <CardFooter>
-                  <div className="chart-legend">
-                    <i className="fa fa-circle text-info" /> Tesla Model S{" "}
-                    <i className="fa fa-circle text-warning" /> BMW 5 Series
-                  </div>
-                  <hr />
-                  <div className="card-stats">
-                    <i className="fa fa-check" /> Data information certified
-                  </div>
-                </CardFooter>
-              </Card>
+            <Col xs="6" sm="2">
+              <BukuRekomendasi 
+                image = "https://upload.wikimedia.org/wikipedia/id/c/c8/Doraemon_volume_1_cover.jpg"
+                kategori = "Komik"
+                genre = "Series"
+                judul = "Doraemon Volume 1"
+              />
+            </Col>
+            <Col xs="6" sm="2">
+              <BukuRekomendasi 
+                image = "https://images.wallpaperscraft.com/image/book_bouquet_cup_147482_3840x2160.jpg"
+                kategori = "Novel"
+                genre = "Remaja"
+                judul = "Cinta Pertamaku"
+              />
+            </Col>
+            <Col xs="6" sm="2">
+              <BukuRekomendasi 
+                image = "https://upload.wikimedia.org/wikipedia/id/c/c8/Doraemon_volume_1_cover.jpg"
+                kategori = "Komik"
+                genre = "Series"
+                judul = "Doraemon Volume 1"
+              />
+            </Col>
+            <Col xs="6" sm="2">
+              <BukuRekomendasi 
+                image = "https://upload.wikimedia.org/wikipedia/id/thumb/8/8e/Laskar_pelangi_sampul.jpg/220px-Laskar_pelangi_sampul.jpg"
+                kategori = "Novel"
+                genre = "Sosial Pendidikan"
+                judul = "Laskar Pelangi"
+              />
+            </Col>
+            <Col xs="6" sm="2">
+              <BukuRekomendasi 
+                image = "https://upload.wikimedia.org/wikipedia/id/c/c8/Ayat-Ayat_Cinta.jpg"
+                kategori = "Novel"
+                genre = "Romantis"
+                judul = "Ayat-ayat Cinta"
+              />
             </Col>
           </Row>
         </div>
